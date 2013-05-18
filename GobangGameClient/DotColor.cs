2d8ccfbb9ccba37706311388a897a5b8f4c4191d
LiaoHaojie <liaohaojie@126.com>
2013-05-18 13:35:12 +0800

    first edition

---
+++ GobangGameClient/DotColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GobangGameClient
+{
+    public enum DotColor
+    {
+        /// <summary>
+        /// 无棋子
+        /// </summary>
+        None = -1,
+        /// <summary>
+        /// 黑色棋子
+        /// </summary>
+        Black = 0,
+        /// <summary>
+        /// 白色棋子
+        /// </summary>
+        White = 1
+    }
+}
